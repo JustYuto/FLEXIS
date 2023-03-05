89c569ed07d0d12482314634f5ca6cc2df0c854c
SebChia <104203508+SebChia@users.noreply.github.com>
2023-03-05 11:10:52 +0800

    Update supervisorHome.php

--- supervisorHome.php
+++ supervisorHome.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
 
+    <body>
+        <h1>Welcome to the Supervisor Page</h1>
+
+        <a href="selectEmpPage.php">Review FWA Request</a>
+</body>
+</html>
