7106988e6c7411ff4dce6e7119658a8b12529284
SebChia <104203508+SebChia@users.noreply.github.com>
2023-03-05 11:09:56 +0800

    Update employeeHome.php

--- employeeHome.php
+++ employeeHome.php
@@ -1,1 +1,21 @@
+<?php
+        if (isset($_POST['submission'])){
+            header("location:submissionPage.php");
+        }
 
+    ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+    
+        <h1>Welcome to the Employee Page</h1>
+        <form action="employeeHome.php" method="post">
+        <input type="submit" name="submission" value="Submit FWA Request">
+    </form>
+</body>
+</html>
