d59e97752f1ea4d953f890147f7f4ed152d3a96c
SebChia <104203508+SebChia@users.noreply.github.com>
2023-03-05 11:11:24 +0800

    Update hrAdminHome.php

--- hrAdminHome.php
+++ hrAdminHome.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
 
+    <body>
+        <h1>Welcome to the HR Admin page</h1>
+
+        <a href="selectDeptPage.php">Record New Employee</a>
+</body>
+</html>
