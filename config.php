0a8a1a0ecfc3260d2242e06eef9a3f8e239f6f76
github <github>
2023-03-08 21:58:32 +0800

    config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$dbname = "FlexIS";
+$dbname = "flexis";
 
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 if($conn->connect_error){
