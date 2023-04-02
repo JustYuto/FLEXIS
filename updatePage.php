fba76c252d273ebb224e56972e4d4558f2e67386
Yuto Yamauchi <yuto.y.malaysia@gmail.com>
2023-04-03 02:15:59 +0800

    Done Boostrap

--- updatePage.php
+++ updatePage.php
@@ -35,8 +35,9 @@ if(isset($_POST['cancel'])){
 <html>
 <?php include 'Component/head.php'; ?>
 <?php include 'Component/header.php'; ?>
+
 <head>
-<script type="text/javascript">
+    <script type="text/javascript">
     function preventBack() {
         window.history.forward()
     };
@@ -46,34 +47,38 @@ if(isset($_POST['cancel'])){
     }
     </script>
 </head>
+
 <body>
     <h1>Update DAILY SCHEDULE</h1>
-    <div class="container">     
-        <div class="center">
-        <form action ="" method = "POST">
-        <label for="date">Date:</label>
-            <input type="date" name="date" value = "date">
-            <label for="workLocation">Work Location: </label>
-            <select id="workLocation" name="workLocation">
-                <option value="Office (Flexi hours)">Office (Flexi hours)</option>
-                <option value="Home (Work from home)">Home (Work from home)</option>
-                <option value="Office (Hybrid)">Office (Hybrid)</option>
-                <option value="Home (Hybrid)">Home (Hybrid)</option>
-            </select><br>
-            <label for="workHours">Work Hours: </label>
-            <select id="workHours" name="workHours">
-                <option value="8:00 a.m. - 4:00 p.m.">8:00 a.m. - 4:00 p.m.</option>
-                <option value="9:00 a.m. - 5:00 p.m.">9:00 a.m. - 5:00 p.m.</option>
-                <option value="10:00 a.m. - 6:00 p.m.">10:00 a.m. - 6:00 p.m.</option>
-            </select><br>
-            <label for="workReport">Work Report:</label>
-                <textarea type="text" name="workReport" placeholder="Write your work report here" rows="4" cols="50" required></textarea><br>
-            <div class="button-group">  
-                <input type="submit" name="update" value = "Update">
-                <input type="submit" name="cancel" value = "Cancel" formnovalidate>
-            </div>  
-        </form>
+    <div class="container">
+        <div class="form-horizontal">
+            <form action="" method="POST">
+                <label class="control-label" for="date">Date</label>
+                <input type="date" class="form-control" id="date" name="date"
+                    value="<?php echo isset($_POST['date']) ? $_POST['date'] : ''; ?>">
+                <label for="workLocation">Work Location: </label>
+                <select id="workLocation" name="workLocation">
+                    <option value="Office (Flexi hours)">Office (Flexi hours)</option>
+                    <option value="Home (Work from home)">Home (Work from home)</option>
+                    <option value="Office (Hybrid)">Office (Hybrid)</option>
+                    <option value="Home (Hybrid)">Home (Hybrid)</option>
+                </select>
+                <label for="workHours">Work Hours: </label>
+                <select id="workHours" name="workHours">
+                    <option value="8:00 a.m. - 4:00 p.m.">8:00 a.m. - 4:00 p.m.</option>
+                    <option value="9:00 a.m. - 5:00 p.m.">9:00 a.m. - 5:00 p.m.</option>
+                    <option value="10:00 a.m. - 6:00 p.m.">10:00 a.m. - 6:00 p.m.</option>
+                </select>
+                <label for="workReport">Work Report:</label>
+                <textarea type="text" name="workReport" placeholder="Write your work report here" rows="4" cols="50"
+                    required></textarea>
+                <div class="button-group">
+                    <input class="btn btn-secondary" type="submit" name="update" value="Update">
+                    <input class="btn btn-secondary" type="submit" name="cancel" value="Cancel" formnovalidate>
+                </div>
+            </form>
         </div>
     </div>
 </body>
+
 </html>
